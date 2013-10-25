68ef474aab6c000eaf9f6ce29756dbbfe7a534cb
Andrew Woodward <awoodward@mirantis.com>
2013-10-25 12:09:33 -0700

    Add ceph::repo RedHat support
    Adds repo's required to support RedHat distros.
    Update case tests so RedHat doesn't fail.
    
    Sourced from: https://github.com/Mirantis/fuel/blob/master/deployment/puppet/ceph/manifests/yum.pp
    
    System spec tests will be provided in a follow up commit
    repos are from a working deployment and are tested by hand.
    
    Closes-bug: #1243872
    Change-Id: I6d9e356bac6855fd30c522916bf7c4867195488c

--- spec/classes/ceph_repo_spec.rb
+++ spec/classes/ceph_repo_spec.rb
@@ -14,6 +14,7 @@
 #
 # Author: François Charlier <francois.charlier@enovance.com>
 # Author: David Moreau Simard <dmsimard@iweb.com>
+# Author: Andrew Woodward <xarses>
 
 require 'spec_helper'
 
@@ -94,6 +95,69 @@ describe 'ceph::repo' do
         :location => 'http://ceph.com/debian-dumpling/',
         :release  => 'precise',
         :require  => 'Apt::Key[ceph]'
+      ) }
+    end
+
+  end
+
+  describe 'RHEL6' do
+
+    let :facts do
+    {
+      :osfamily         => 'RedHat',
+    }
+    end
+
+    describe "with default params" do
+
+      it { should contain_yumrepo('ext-epel-6.8').with(
+        :descr      => 'External EPEL 6.8',
+        :name       => 'ext-epel-6.8',
+        :gpgcheck   => '0',
+        :mirrorlist => 'https://mirrors.fedoraproject.org/metalink?repo=epel-6&arch=${basearch}'
+      ) }
+
+      it { should contain_yumrepo('ext-ceph').with(
+        :descr      => 'External Ceph cuttlefish',
+        :name       => 'ext-ceph-cuttlefish',
+        :baseurl    => 'http://ceph.com/rpm-cuttlefish/el6/${basearch}',
+        :gpgcheck   => '1',
+        :gpgkey     => 'https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/release.asc'
+     ) }
+
+      it { should contain_yumrepo('ext-ceph-noarch').with(
+        :descr      => 'External Ceph noarch',
+        :name       => 'ext-ceph-cuttlefish-noarch',
+        :baseurl    => 'http://ceph.com/rpm-cuttlefish/el6/noarch',
+        :gpgcheck   => '1',
+        :gpgkey     => 'https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/release.asc'
+      ) }
+
+
+
+    end
+
+    describe "when overriding ceph release" do
+      let :params do
+        {
+         :release => 'dumpling'
+        }
+      end
+
+      it { should contain_yumrepo('ext-ceph').with(
+        :descr      => 'External Ceph dumpling',
+        :name       => 'ext-ceph-dumpling',
+        :baseurl    => 'http://ceph.com/rpm-dumpling/el6/${basearch}',
+        :gpgcheck   => '1',
+        :gpgkey     => 'https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/release.asc'
+     ) }
+
+      it { should contain_yumrepo('ext-ceph-noarch').with(
+        :descr      => 'External Ceph noarch',
+        :name       => 'ext-ceph-dumpling-noarch',
+        :baseurl    => 'http://ceph.com/rpm-dumpling/el6/noarch',
+        :gpgcheck   => '1',
+        :gpgkey     => 'https://ceph.com/git/?p=ceph.git;a=blob_plain;f=keys/release.asc'
       ) }
     end
 
