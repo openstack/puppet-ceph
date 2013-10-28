65814957a5c9286ca4681a1b9981fa8612fbc944
Loic Dachary <loic@dachary.org>
2013-10-28 21:16:37 +0100

    ceph::repo integration tests
    ceph::repo supports cuttlefish + dumpling on debian. Checking that
    apt-cache policy returns the correct version shows the key was properly
    installed and matches the repository. Omitting the release parameter
    also checks that the default is cuttlefish.
    
    Closes-bug: #1245177
    
    Change-Id: I01ea92798db5d6d74507f247d3e49a93d1f0fc19
    Signed-off-by: Loic Dachary <loic@dachary.org>

---
+++ spec/system/ceph_repo_spec.rb
@@ -0,0 +1,67 @@
+#
+#  Copyright 2013 Cloudwatt <libre-licensing@cloudwatt.com>
+#
+#  Author: Loic Dachary <loic@dachary.org>
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+require 'spec_helper_system'
+
+describe 'ceph::repo' do
+
+  describe 'dumpling' do
+    it 'should find 0.67' do
+      pp = <<-EOS
+        class { 'ceph::repo':
+          release => 'dumpling'
+        }
+      EOS
+
+      # Run it twice and test for idempotency
+      puppet_apply(pp) do |r|
+        r.exit_code.should_not == 1
+        r.refresh
+        r.exit_code.should be_zero
+      end
+
+    end
+
+    context shell 'apt-cache policy ceph' do
+      its(:stdout) { should =~ /Candidate: 0.67/ }
+      its(:stderr) { should be_empty }
+      its(:exit_code) { should be_zero }
+    end
+  end
+
+  describe 'cuttlefish (default)' do
+    it 'should find 0.61' do
+      pp = <<-EOS
+        class { 'ceph::repo': }
+      EOS
+
+      # Run it twice and test for idempotency
+      puppet_apply(pp) do |r|
+        r.exit_code.should_not == 1
+        r.refresh
+        r.exit_code.should be_zero
+      end
+
+    end
+
+    context shell 'apt-cache policy ceph' do
+      its(:stdout) { should =~ /Candidate: 0.61/ }
+      its(:stderr) { should be_empty }
+      its(:exit_code) { should be_zero }
+    end
+  end
+end
