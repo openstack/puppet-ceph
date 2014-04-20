7439be3bb82e81fd33e5f106b7ca61abbd6c21ea
Loic Dachary <loic@dachary.org>
2014-04-21 01:24:45 +0200

    ceph::key : update copyright notice
    To reflect the latest contributions.
    
    Change-Id: If21277baa0e7baa4534e7498cd38da9982023e77
    Signed-off-by: Loic Dachary <loic@dachary.org>

--- spec/system/ceph_key_spec.rb
+++ spec/system/ceph_key_spec.rb
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2014 Catalyst IT Limited.
+# Copyright (C) 2014 Cloudwatt <libre-licensing@cloudwatt.com>
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -14,6 +15,7 @@
 #   limitations under the License.
 #
 # Author: Ricardo Rocha <ricardo@catalyst.net.nz>
+# Author: Loic Dachary <loic@dachary.org>
 #
 
 require 'spec_helper_system'
