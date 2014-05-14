05d23fe01b66870e4b5d7805ca2601b7d2f96d75
David Gurtner <david@nine.ch>
2014-05-14 14:28:53 +0200

    Adding roles
    The Roles are ready for inclusion from node manifests or ENCs
    You might want to have a look at the hiera files under examples/

---
+++ manifests/role/base.pp
@@ -0,0 +1,31 @@
+class ceph::profile::base {
+  $release = hiera('ceph::release')
+  $fsid = hiera('ceph::conf::fsid')
+  $authentication_type = hiera('ceph::conf::authentication_type')
+  $osd_pool_default_pg_num = hiera('ceph::conf::osd_pool_default_pg_num')
+  $osd_pool_default_pgp_num = hiera('ceph::conf::osd_pool_default_pgp_num')
+  $osd_pool_default_size = hiera('ceph::conf::osd_pool_default_size')
+  $osd_pool_default_min_size = hiera('ceph::conf::osd_pool_default_min_size')
+  $mon_initial_members = hiera('ceph::conf::mon_initial_members')
+  $mon_host = hiera('ceph::conf::mon_host')
+  $cluster_network = hiera('ceph::conf::cluster_network')
+  $public_network = hiera('ceph::conf::public_network')
+
+  class { 'ceph::repo':
+    release => $release,
+  } ->
+
+  class { 'ceph':
+    fsid                      => $fsid,
+    authentication_type       => $authentication_type,
+    osd_pool_default_pg_num   => $osd_pool_default_pg_num,
+    osd_pool_default_pgp_num  => $osd_pool_default_pgp_num,
+    osd_pool_default_size     => $osd_pool_default_size,
+    osd_pool_default_min_size => $osd_pool_default_min_size,
+    mon_initial_members       => $mon_initial_members,
+    mon_host                  => $mon_host,
+    cluster_network           => $cluster_network,
+    public_network            => $public_network,
+  }
+
+}
