d94c7754a25da674047ebee1906f554a760d0da3
MAZEN Nicolas <mazen.nico@gmail.com>
2014-06-17 16:01:09 +0200

    The year missing in Copyright is add
    Closes-bug: #1330987
    Change-Id: I8625c4f0b9733b96d8c54aac76339c7b50eb9ef4

--- spec/classes/ceph_mds_spec.rb
+++ spec/classes/ceph_mds_spec.rb
@@ -1,4 +1,4 @@
-#   Copyright (C) iWeb Technologies Inc.
+#   Copyright (C) 2013, 2014 iWeb Technologies Inc.
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
