8f9dfd6681c515fe86d15918aa0448937272a2e5
th0rnspear <th0rnspear@yopmail.com>
2014-06-25 12:11:30 +0200

    Changed 'colour' to 'color' in /spec/spec_helper_system.rb
    Closes-Bug: #1334136
    Change-Id: I4af92b992210a9241441c33b97ce796e81ecd59d

--- spec/spec_helper_system.rb
+++ spec/spec_helper_system.rb
@@ -26,7 +26,7 @@ RSpec.configure do |c|
   # Project root
   proj_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 
-  # Enable colour
+  # Enable color
   c.tty = true
 
   c.include RSpecSystemPuppet::Helpers
