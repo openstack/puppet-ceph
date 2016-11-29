5a47d93fd5e2aed013ce636a4d053567723ceb04
Mykyta Karpin <mkarpin@mirantis.com>
2016-11-29 15:11:03 +0200

    Use facts from puppet-openstack_spec_helper
    rspec-puppet-facts are available from puppet-openstack_spec_helper/facts
    so we can use it now
    
    Change-Id: If30bf758d539d4becd970a63053ac1e5d55d9ec4

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,19 +2,11 @@
 $LOAD_PATH.push(File.join(File.dirname(__FILE__), 'fixtures', 'modules', 'openstacklib', 'lib'))
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'shared_examples'
-
-require 'puppet-openstack_spec_helper/defaults'
-require 'rspec-puppet-facts'
-include RspecPuppetFacts
+require 'puppet-openstack_spec_helper/facts'
 
 RSpec.configure do |c|
   c.alias_it_should_behave_like_to :it_configures, 'configures'
   c.alias_it_should_behave_like_to :it_raises, 'raises'
-  # TODO(aschultz): remove this after all tests converted to use OSDefaults
-  # instead of referencing @default_facts
-  c.before :each do
-    @default_facts = OSDefaults.get_facts
-  end
   fixture_path = File.expand_path(File.join(File.dirname(__FILE__), 'fixtures'))
 
   c.hiera_config = File.join(fixture_path, 'hieradata/hiera.yaml')
