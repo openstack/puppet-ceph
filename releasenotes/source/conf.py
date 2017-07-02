3850c05ddd84c5a10653ae7e1653f8c9d1146c57
ZhongShengping <chdzsp@163.com>
2017-07-02 14:09:58 +0800

    Fix openstackdocstheme
    See the docs[0] how to use it. Also add the variables for the bug
    log feature.
    
    [0]https://docs.openstack.org/openstackdocstheme/latest/
    
    Change-Id: I2d150f9db7a117c20d38a29bd4415995ec752e1f

--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -20,7 +20,6 @@
 
 # -- General configuration ------------------------------------------------
 
-import openstackdocstheme
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #needs_sphinx = '1.0'
@@ -29,6 +28,7 @@ import openstackdocstheme
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'openstackdocstheme',
     'reno.sphinxext',
 ]
 
@@ -108,7 +108,7 @@ html_theme = 'openstackdocs'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = [openstackdocstheme.get_html_theme_path()]
+# html_theme_path = []
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
@@ -138,7 +138,7 @@ html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
-#html_last_updated_fmt = '%b %d, %Y'
+html_last_updated_fmt = '%Y-%m-%d %H:%M'
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
@@ -261,3 +261,8 @@ texinfo_documents = [
 
 # -- Options for Internationalization output ------------------------------
 locale_dirs = ['locale/']
+
+# openstackdocstheme options
+repository_name = 'openstack/puppet-ceph'
+bug_project = 'puppet-ceph'
+bug_tag = ''
