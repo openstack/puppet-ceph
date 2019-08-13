a09926aa2d6c0127ca13eeb94303680c5eac86ea
ZhongShengping <chdzsp@163.com>
2019-08-13 11:29:06 +0800

    Bump the openstackdocstheme extension to 1.20
    Some options are now automatically configured by the version 1.20[1]:
    - project
    - html_last_updated_fmt
    - latex_engine
    - latex_elements
    
    [1]https://docs.openstack.org/openstackdocstheme/latest/#using-the-theme
    
    Change-Id: I514c232a0f0d0ca47031a13b325bbc98d2ca62db

--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -45,7 +45,6 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'puppet-ceph Release Notes'
 copyright = u'2016, Puppet OpenStack Developers'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -138,7 +137,6 @@ html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
-html_last_updated_fmt = '%Y-%m-%d %H:%M'
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
@@ -182,17 +180,6 @@ htmlhelp_basename = 'puppet-cephReleaseNotesdoc'
 
 
 # -- Options for LaTeX output ---------------------------------------------
-
-latex_elements = {
-# The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
-
-# The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
-
-# Additional stuff for the LaTeX preamble.
-#'preamble': '',
-}
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
