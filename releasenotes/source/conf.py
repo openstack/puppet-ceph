105b71e7b94df36e10803a8b4145c165d1e26c83
ZhongShengping <chdzsp@163.com>
2020-09-16 11:05:05 +0800

    Set openstackdocs_auto_name to use 'project' as name
    Change-Id: I18c16b5284b1e1a8e30e69a45ce6c6b315adf918

--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -253,3 +253,4 @@ locale_dirs = ['locale/']
 openstackdocs_repo_name = 'openstack/puppet-ceph'
 openstackdocs_bug_project = 'puppet-ceph'
 openstackdocs_bug_tag = ''
+openstackdocs_auto_name = False
