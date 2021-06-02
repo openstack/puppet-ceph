37b0c207dfeeff8a9e57ad444fa36e39358efc95
Takashi Kajinami <tkajinam@redhat.com>
2021-06-02 13:49:13 +0900

    Set fixture paths for unit tests
    This change defines manifest_dir and module_path expicitly in unit
    tests so that modules installed under fixtures directory is properly
    loaded.
    
    Closes-Bug: #1930403
    Change-Id: I3bb2b082e33c2843abcb97b0ae28b04dc33ac1ec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,12 +4,15 @@ require 'puppetlabs_spec_helper/module_spec_helper'
 require 'shared_examples'
 require 'puppet-openstack_spec_helper/facts'
 
+fixture_path = File.expand_path(File.join(File.dirname(__FILE__), 'fixtures'))
+
 RSpec.configure do |c|
   c.alias_it_should_behave_like_to :it_configures, 'configures'
   c.alias_it_should_behave_like_to :it_raises, 'raises'
-  fixture_path = File.expand_path(File.join(File.dirname(__FILE__), 'fixtures'))
 
   c.hiera_config = File.join(fixture_path, 'hieradata/hiera.yaml')
+  c.module_path = File.join(fixture_path, 'modules')
+  c.manifest_dir = File.join(fixture_path, 'manifests')
 
   c.before(:all) do
     data = YAML.load_file(c.hiera_config)
