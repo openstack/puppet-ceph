6e154aee1487c64f999bc46e2842efd4004bbe8b
ZhongShengping <chdzsp@163.com>
2021-09-30 13:38:32 +0800

    Delete useless code for setup.py
    Change-Id: I81eb9af7a7e9c200e8141b8c4b7dc91830397faf

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # Copyright (c) 2013 Hewlett-Packard Development Company, L.P.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# THIS FILE IS MANAGED BY THE GLOBAL REQUIREMENTS REPO - DO NOT EDIT
 import setuptools
 
 setuptools.setup(
