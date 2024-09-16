f695c87de93d50609b48088d51d3904048aa9399
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-09-17 00:38:25 +0900

    Centralize LOAD_PATH modification
    Change-Id: I25ad088a159fdaa344b0c68c870440160e918c9a

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
-# Load libraries from openstacklib here to simulate how they live together in a real puppet run (for provider unit tests)
+# Load libraries here to simulate how they live together in a real puppet run (for provider unit tests)
+$LOAD_PATH.push(File.join(File.dirname(__FILE__), 'fixtures', 'modules', 'initile', 'lib'))
 $LOAD_PATH.push(File.join(File.dirname(__FILE__), 'fixtures', 'modules', 'openstacklib', 'lib'))
+
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'shared_examples'
 require 'puppet-openstack_spec_helper/facts'
